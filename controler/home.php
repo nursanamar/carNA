f39332f41a649dcbf8010263bbd4a1b76330949c
default_git_name <default_git_email>
2016-12-26 01:26:23 +0000

    ubah kontroler

--- controler/home.php
+++ controler/home.php
@@ -26,8 +26,9 @@
 			echo $res;
 		}
 		public function ubah(){
-			$data = array("kelas" => "XII TKJ 1");
-			$this->model->ubah($data);
+			$data = array("nama" => $_POST['nama'],"kelas" => $_POST['kelas']);
+			$where = $_POST['where'];
+			$this->model->ubah($data,$where);
 		}
 		public function hapus(){
 			$this->model->hapus("nama",$_POST['nama']);
