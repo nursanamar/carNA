4bb80b1b5b263580ca994044094a6421fae1c1dd
default_git_name <default_git_email>
2016-12-27 07:02:57 +0000

    database

--- config/db.php
+++ config/db.php
@@ -2,10 +2,10 @@
 	namespace config	;
 	class db{
 		public function config(){
-			$config['host'] = "localhost";
-			$config['dbname'] = "framework";
-			$config['user'] = "root";
-			$config['pass'] = "";
+			$config['host'] = "mysql.hostinger.com";
+			$config['dbname'] = "u968639921_carna";
+			$config['user'] = "u968639921_carna";
+			$config['pass'] = "carna123";
 			return $config;
 		}
 	}
